668b10455258a301095207407b810bca075c633f
Hamza FELLAH <fellahhamza48@gmail.com>
2025-03-26 02:24:58 +0000

    updating the ServiceType

--- backend/src/main/java/com/fssm/worldcup/Models/General/ServiceType.java
+++ backend/src/main/java/com/fssm/worldcup/Models/General/ServiceType.java
@@ -1,48 +1,19 @@
-// src/main/java/com/fssm/worldcup/Models/General/ServiceType.java
 package com.fssm.worldcup.Models.General;
 
 import jakarta.persistence.*;
 import lombok.*;
 
-import java.util.List;
-
 @Entity
-@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
+@Builder
 public class ServiceType {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer serviceTypeId;
     private String serviceTypeName;
-
-    @ManyToMany(mappedBy = "serviceTypes")
-    private List<Provider> providers;
-
-    // Getters and Setters
-    public Integer getServiceTypeId() {
-        return serviceTypeId;
-    }
-
-    public void setServiceTypeId(Integer serviceTypeId) {
-        this.serviceTypeId = serviceTypeId;
-    }
-
-    public String getServiceTypeName() {
-        return serviceTypeName;
-    }
-
-    public void setServiceTypeName(String serviceTypeName) {
-        this.serviceTypeName = serviceTypeName;
-    }
-
-    public List<Provider> getProviders() {
-        return providers;
-    }
-
-    public void setProviders(List<Provider> providers) {
-        this.providers = providers;
-    }
+    @ManyToOne
+    private Provider provider;
 }
